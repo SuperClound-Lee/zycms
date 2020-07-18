46654ae3ebe254a604f9a8500dbd104756dd665c
Floweremmm <1823467794@qq.com>
2020-07-18 14:22:53 +0800

    项目更新

--- src/main/java/com/zydata/controller/master/MasterController.java
+++ src/main/java/com/zydata/controller/master/MasterController.java
@@ -1,14 +1,17 @@
 package com.zydata.controller.master;
 
+import com.zydata.utils.common.validationCodeUtil;
 import jdk.nashorn.internal.runtime.logging.Logger;
 import lombok.extern.java.Log;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 管理员控制器
@@ -41,4 +44,25 @@ public class MasterController {
         return "admin/index";
 
     }
+
+    @RequestMapping("/createImg")
+    public void createImg(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        try {
+            response.setContentType("image/jpeg");//设置相应类型,告诉浏览器输出的内容为图片
+            response.setHeader("Pragma", "No-cache");//设置响应头信息，告诉浏览器不要缓存此内容
+            response.setHeader("Cache-Control", "no-cache");
+            response.setDateHeader("Expire", 0);
+            validationCodeUtil randomValidateCode = new validationCodeUtil();
+            randomValidateCode.getRandcode(request, response);//输出验证码图片
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+        //从session中取出随机验证码
+        System.out.println(request.getSession().getAttribute("RANDOMREDISKEY"));
+    }
+
+
+
 }
+
+
