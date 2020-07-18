1a179b266c253326616ec362b79373cf6ec5f2a6
Floweremmm <1823467794@qq.com>
2020-07-18 14:39:54 +0800

    提示信息

--- src/main/java/com/zydata/config/MasterConfigMessage.java
+++ src/main/java/com/zydata/config/MasterConfigMessage.java
@@ -7,16 +7,13 @@ import org.springframework.context.annotation.Configuration;
  * 提示信息
  */
 public class MasterConfigMessage {
-
-     static final String LOGIN_SUCCESS="登录成功！";
-     static final String LOGIN_ERROR="用户名或密码错误";
-     static final String VALIDATE_USERNAME="用户名为6-20位中文、数字、字母任意组合";
-     static final String VALIDATE_PASSWORD="密码为8-16位以字母开头、数字、特殊字符组合";
-     static final String VALIDATE_EMAIL="邮箱格式不正确";
-     static final String VALIDATE_PHONE="手机格式不正确";
-     static final String MASTER_ADD_SUCCESS="添加%s失败！用户已存在";
-     static final String MASTER_ADD_ERROR="添加%s用户成功";
-
-
+    public static final String LOGIN_SUCCESS="登录成功！";
+    public static final String LOGIN_ERROR="用户名或密码错误";
+    public static final String VALIDATE_USERNAME="用户名为6-20位中文、数字、字母任意组合";
+    public static final String VALIDATE_PASSWORD="密码为8-16位以字母开头、数字、特殊字符组合";
+    public static final String VALIDATE_EMAIL="邮箱格式不正确";
+    public static final String VALIDATE_PHONE="手机格式不正确";
+    public static final String MASTER_ADD_SUCCESS="添加%s失败！用户已存在";
+    public static final String MASTER_ADD_ERROR="添加%s用户成功";
 
 }
